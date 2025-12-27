310813ce664550fe8a446bb304d6e9a07b1510eb
Mansur@Apple <raspberryjar@gmail.com>
2025-12-27 15:44:38 -0800

    Add 3Sum code

---
+++ 3Sum_problem.py
@@ -0,0 +1,38 @@
+from typing import List
+
+def threeSum(nums: List[int]) -> List[List[int]]:
+    nums.sort()
+    res: List[List[int]] = []
+    n = len(nums)
+    for i in range(len(nums) - 2):
+        if nums[i] > 0:
+            break
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue
+
+        left, right = i + 1, n - 1
+        while left < right:
+            s = nums[i] + nums[left] + nums[right]
+            if s < 0:
+                left += 1
+            elif s > 0:
+                right -= 1
+            else:
+                res.append([nums[i], nums[left], nums[right]])
+                left += 1
+                right -= 1
+                while left < right and nums[left] == nums[left -1]:
+                    left +=1
+                while left < right and nums[right] == nums[right + 1]:
+                    right -= 1
+                left += 1
+                right -= 1
+    return res
+
+if __name__ == "__main__":
+    nums = [-1, 0, 1, 2, -1, -4]
+    print(threeSum(nums))
+    nums1 = [-4, -1, -1, 0, 1, 2]
+    print(threeSum(nums1))
+    nums2 = [-1, 0, 1, 2, -1, -4]
+    print(threeSum(nums2))
