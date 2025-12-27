013e97cefbbf908c7bbb15739969dfef184e57cd
Mansur@Apple <raspberryjar@gmail.com>
2025-12-27 15:47:25 -0800

    Add 3Sum code explanation

--- 3Sum_problem.py
+++ 3Sum_problem.py
@@ -1,3 +1,9 @@
+"""
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+Notice that the solution set must not contain duplicate triplets.
+Given an interger array, find all unique triplets that sum up to zero = a+b+c=0.
+"""
+
 from typing import List
 
 def threeSum(nums: List[int]) -> List[List[int]]:
