a850873948ae4d323186aba853c1babc7ef36d8e
Mansur@Apple <raspberryjar@gmail.com>
2025-12-27 21:31:08 -0800

    arithmetic operations

---
+++ arithmeticOperations.py
@@ -0,0 +1,9 @@
+x = 10
+y = 3
+print("Sum", x + y)
+print("Difference", x - y)
+print("Product", x * y)
+print("Division", x / y)
+print("Floor Division", x // y)
+print("Remainder, Modulus", x % y)
+print("Power", x ** y)
